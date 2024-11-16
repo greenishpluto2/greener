a6558e336f7547b21f8e66785fff9054134cb6c9
GreenishPluto <greenishpluto2@gmail.com>
2024-11-16 15:28:16 +0200

    deployment setting change

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // fixes wallet connect dependency issue https://docs.walletconnect.com/web3modal/nextjs/about#extra-configuration
   webpack: (config) => {
     config.externals.push("pino-pretty", "lokijs", "encoding");
     return config;
   },
+  env: {
+    NEXT_PUBLIC_TEMPLATE_CLIENT_ID: process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID,
+},
 };
 
 export default nextConfig;
